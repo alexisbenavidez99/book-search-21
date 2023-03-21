46c8c61a8d64834a283c0fc649c4a9cda5eea081
alexisbenavidez99 <callmealexis49@gmail.com>
2023-03-20 20:45:06 -0500

    added apollo/client package, added Auth type and mutations to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,6 +7,30 @@ const resolvers = {
         }
     },
     Mutation: {
-        
-    }
-}
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        },
+        saveBook: async (parent, { user, body }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+              );
+              return updatedUser;
+        },
+        login: async (parent, args, { body }) => {
+            const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+        },
+        removeBook: async (parent, { user, params }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+              );
+              return updatedUser;
+        },
+    },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,6 +28,11 @@ type Book {
     image: String
     link: String
 }
+
+type Auth {
+    token: ID!
+    user: User
+}
 `;
 
 module.exports = typeDefs;
